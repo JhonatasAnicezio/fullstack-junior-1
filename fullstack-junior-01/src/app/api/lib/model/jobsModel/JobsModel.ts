221846e5a473ad53f84a53fab8d18cc3ea083297
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-04-12 12:39:27 -0300

    feat: create layer model for jobs

--- fullstack-junior-01/src/app/api/lib/model/jobsModel/JobsModel.ts
+++ fullstack-junior-01/src/app/api/lib/model/jobsModel/JobsModel.ts
@@ -5,34 +5,7 @@ export async function getJobs() {
     try {
         return jobs;
     } catch (error) {
-        console.error(`Erro na leitura do arquivo ${error}`);
-    }
-}
-
-export async function getJobsByLevel(level: string) {
-    try {
-        const jobsJuniors = jobs.filter((job) => job.level === level)
-
-        return jobsJuniors;
-    } catch (error) {
-        console.error(`Erro na leitura do arquivo ${error}`);
-    }
-}
-
-export async function getJobById(id: number) {
-    try {
-        const job = jobs.filter((job) => job.id === id)
-
-        return job;
-    } catch (error) {
-        console.error(`Erro na leitura do arquivo ${error}`);
-    }
-}
-
-export async function postJobSubmit({ name, age, phone, state, city }: IPerson) {
-    return {
-        message: `Thank you for your
-        application, ${name}`
+        throw new Error('Erro na leitura de jobs');
     }
 }
 
