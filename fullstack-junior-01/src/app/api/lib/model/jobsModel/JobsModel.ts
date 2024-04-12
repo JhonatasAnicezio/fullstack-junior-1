3788d7e14b253b49dd6dfb4234998cf49aaa8552
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2024-04-12 12:46:41 -0300

    feat: create in layer model, function getJobsByLevel

--- fullstack-junior-01/src/app/api/lib/model/jobsModel/JobsModel.ts
+++ fullstack-junior-01/src/app/api/lib/model/jobsModel/JobsModel.ts
@@ -9,6 +9,17 @@ export async function getJobs() {
     }
 }
 
+export async function getJobsByLevel(level: string) {
+    try {
+        const jobsJuniors = jobs.filter((job) => job.level === level)
+
+        return jobsJuniors;
+    } catch (error) {
+        throw new Error('Erro na leitura de jobs a partir do level');
+    }
+}
+
 export const JobsModel = {
     getJobs,
+    getJobsByLevel,
 }
